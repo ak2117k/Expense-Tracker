71dd875cf04668cb012f14762a00e4745e7410c0
Aryan Kumar <ak.2117k@gmail.com>
2025-06-23 17:56:32 +0530

    updates

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -6,6 +6,6 @@ export const axiosInstance = axios.create({
   //     ? "http://localhost:4000/api/v1"
   //     : "/api/v1",
 
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://expense-tracker-37n6.onrender.com/api/v1",
   withCredentials: true,
 });
